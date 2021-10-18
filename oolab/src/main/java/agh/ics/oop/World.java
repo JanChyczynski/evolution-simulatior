62d42c950d8747d7cdbc1c232b9c7279bddea03b
jchyczynski <jasiek.ch16@gmail.com>
2021-10-18 11:10:13 +0200

    first commit, simple main and run

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,12 @@
+package agh.ics.oop;
+
+public class World {
+    static void run(){
+        System.out.println("Zwierzak idzie do przodu");
+    }
+    public static void main(String[] args) {
+        System.out.println("Start");
+        run();
+        System.out.println("Stop");
+    }
+}
