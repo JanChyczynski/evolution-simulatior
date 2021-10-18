79f4326e1c3fee4bb75865528c61167d3bd0719a
jchyczynski <jasiek.ch16@gmail.com>
2021-10-18 11:20:04 +0200

    print arguments in run

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,12 +1,21 @@
 package agh.ics.oop;
 
 public class World {
-    static void run(){
+    static void run(String[] args){
         System.out.println("Zwierzak idzie do przodu");
+        for (int i = 0; i < args.length; i++){
+            System.out.print(args[i]);
+            if(i < args.length-1){
+                System.out.print(", ");
+            }
+            else{
+                System.out.println();
+            }
+        }
     }
     public static void main(String[] args) {
         System.out.println("Start");
-        run();
+        run(new String[]{"f", "f"});
         System.out.println("Stop");
     }
 }
