6ffebf6c7bd48b536be82d52b932016a817aa18d
jchyczynski <jasiek.ch16@gmail.com>
2021-11-13 21:50:11 +0100

    SimulationEnginge integration test

---
+++ oolab/src/test/java/agh/ics/oop/SimulationEngineTest.java
@@ -0,0 +1,25 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationEngineTest {
+
+    @Test
+    void run() {
+        IWorldMap map = new RectangularMap(2,2);
+        SimulationEngine engine = new SimulationEngine(
+                new OptionParser().parse(new String[]{"f", "r", "f", "r", "f"}),
+                map, new Vector2d[]{new Vector2d(0, 0), new Vector2d(1, 1)});
+
+        engine.run();
+
+        assert(map.isOccupied(new Vector2d(0, 2)));
+        assert(map.isOccupied(new Vector2d(1, 1)));
+        assertEquals(((Animal)map.objectAt(new Vector2d(0,2))).getPosition(), new Vector2d(0,2));
+        assertEquals(((Animal)map.objectAt(new Vector2d(0,2))).getOrientation(), MapDirection.NORTH);
+        assertEquals(((Animal)map.objectAt(new Vector2d(1, 1))).getPosition(), new Vector2d(1, 1));
+        assertEquals(((Animal)map.objectAt(new Vector2d(1, 1))).getOrientation(), MapDirection.SOUTH);
+    }
+}
